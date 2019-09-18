5855b81636bd1f5343d4778973a6e0cc96c2213f
Bazzan <sebake01@gmail.com>
2019-09-18 17:37:28 +0200

    JS

---
+++ MatteSpel/JS/JS.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+
+    var canvas = document.getElementById("myBox");
+    var context = canvas.getContext("2d");
+    var mathProblem;
+
+    this.alert("yoyoy");
+
+    DrawQuestion = function () {
+        this.alert("i funktion");
+        context.textBaseline = "middle";
+        context.font = "24pt Calibri, Arial";
+        context.fillStyle = "white";
+        context.font = "bold 16pc Arial"
+        context.fillText("hello, 10, 10");
+
+        
+      
+
+}
+
+};
