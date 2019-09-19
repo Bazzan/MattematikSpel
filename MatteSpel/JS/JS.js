38dd2a3190e287c7b39c309d09c507518a925629
Disa <disaviola@hotmail.com>
2019-09-19 15:57:35 +0200

    asdfg

--- MatteSpel/JS/JS.js
+++ MatteSpel/JS/JS.js
@@ -1,21 +1,50 @@
 window.onload = function () {
-
+    var currentMode = "addition";
     var canvas = document.getElementById("myBox");
     var context = canvas.getContext("2d");
     var mathProblem;
+    var equation;
+    
 
-    this.alert("yoyoy");
+    
 
     DrawQuestion = function () {
-        this.alert("i funktion");
-        context.textBaseline = "middle";
-        context.font = "24pt Calibri, Arial";
-        context.fillStyle = "white";
-        context.font = "bold 16pc Arial"
-        context.fillText("hello, 10, 10");
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.beginPath();       
 
+        if(currentMode == "addition"){
+            var temp1 = Math.floor(Math.random() * 100) + 1;
+            var temp2 = Math.floor(Math.random() * 100) + 1;
+            equation = temp1 + "+" + temp2;
+        }
+        if(currentMode == "subtraction"){
+            var temp1 = Math.floor(Math.random() * 100) + 1;
+            var temp2 = Math.floor(Math.random() * 100) + 1;
+            equation = temp1 + "-" + temp2;
+        }
+        if(currentMode == "multiplication"){
+            var temp1 = Math.floor(Math.random() * 100) + 1;
+            var temp2 = Math.floor(Math.random() * 100) + 1;
+            equation = temp1 + "x" + temp2;
+        }
+
+        context.textBaseline = "middle";
+        context.font = "24pt Calibri, Arial";        
+        context.fillStyle = "black";
+        context.fillText("" + equation + "", 50, 30);
         
-      
+        
+    SetAddition = function (){
+        currentMode = "addition";
+    }
+        
+    SetSubtraction = function (){
+        currentMode = "subtraction";
+    }
+
+    SetMultiplication = function (){
+        currentMode = "multiplication";
+    }
 
 }
 
